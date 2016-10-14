c7a5fe69bfdd7ca23c3a0443830a30d8c230c31c
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-10-14 11:57:33 -0400

    Create commentable in controller. Merge comments and questions to one view page.

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,16 +1,35 @@
 get "/answers/:id/comments" do
-    @answer = Answer.find_by(id: params[:id])
+    @commentable = Answer.find_by(id: params[:id])
     erb :"comments/_comment"
 end
 
 
 post "/answers/:id/comments" do
-    @answer = Answer.find_by(id: params[:id])
-    @comment = Comment.new(text: params[:text], commenter: current_user, commentable: @answer)
+    @commentable = Answer.find_by(id: params[:id])
+    @comment = Comment.new(text: params[:text], commenter: current_user, commentable: @commentable)
 
     if @comment.save
       status 200
       redirect '/'
+    else
+     status 500
+     @errors = @comment.errors.full_messages
+     erb :"comments/_comment"
+    end
+end
+
+get "/questions/:id/comments/new" do
+    @commentable = Question.find_by(id: params[:id])
+    erb :"comments/_comment"
+end
+
+post "/questions/:id/comments" do
+    @commentable = Question.find_by(id: params[:id])
+    @comment = Comment.new(text: params[:text], commenter: current_user, commentable: @commentable)
+
+    if @comment.save
+      status 200
+      erb :'/questions/show'
     else
      status 500
      @errors = @comment.errors.full_messages
