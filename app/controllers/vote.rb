ef25a2ae081f3fceb9f0a4e0a7ad05f84eed0979
Jesusmar Lima <jesusmarlima@gmail.com>
2016-10-15 17:15:17 -0400

    add votes for answer comments

--- app/controllers/vote.rb
+++ app/controllers/vote.rb
@@ -72,3 +72,29 @@ post  "/questions/:id/answers/:answer_id/votes/up" do
     erb :'/questions/show'
   end
 end
+
+
+
+post "/questions/:id/answers/:answer_id/comments/:comment_id/votes/down" do
+  @comment =  Comment.find_by(id: params[:comment_id])
+  vote = Vote.new(voteable: @comment, voter: current_user, vote_value: -1 )
+  if vote.save
+    status 200
+    total_votes(@comment).to_s
+  else
+    status 500
+    erb :'/questions/show'
+  end
+end
+
+post  "/questions/:id/answers/:answer_id/comments/:comment_id/votes/up" do
+  @comment =  Comment.find_by(id: params[:comment_id])
+  vote = Vote.new(voteable: @comment, voter: current_user, vote_value: 1 )
+  if vote.save
+    status 200
+    total_votes(@comment).to_s
+  else
+    status 500
+    erb :'/questions/show'
+  end
+end
