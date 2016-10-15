bc8a82b93dd92e72e4273ac65bf3137e52476702
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-10-14 20:09:40 -0400

    Delete commented out code. CLean up file.

--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,3 @@
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-
-
-  // $(".form_comment").submit(function(event){
-  //   event.preventDefault()
-  //   alert(this)
-  //   $ajax({
-  //     method: 'post',
-  //     url: '/comments',
-  //     data: $(this).serialize()
-  //   }).done(function(response){
-  //     alert(response)
-  //   })
-  // })
-
 function hideTabContent() {
   $("#profile").hide();
   $("#activity").hide();
